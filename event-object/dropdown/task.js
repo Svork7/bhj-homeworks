da60a2dad744cc4304749691305e711163a7f618
Eugene Mikhailov <svork@ya.ru>
2022-10-03 18:26:51 +0300

    task1 done

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,6 +1,6 @@
-const dropdownList = document.querySelector('.dropdown__list '),
-dropdownValue  = document.querySelector('.dropdown__value '),
-dropdownItem =  Array.from(document.querySelectorAll('.dropdown__item'));
+const dropdownList = document.querySelector('.dropdown__list ')
+const dropdownValue  = document.querySelector('.dropdown__value ')
+const dropdownItem =  Array.from(document.querySelectorAll('.dropdown__item'));
 
 function dClick (){
   dropdownList.classList.toggle('dropdown__list_active');
