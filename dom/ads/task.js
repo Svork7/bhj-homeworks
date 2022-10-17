f675e6453a2f54dfcf8963a417f5fd33b4954579
Eugene Mikhailov <svork@ya.ru>
2022-10-17 15:50:58 +0300

    task 2

---
+++ dom/ads/task.js
@@ -0,0 +1,15 @@
+const rotatorCase = document.querySelectorAll(".rotator__case");
+let arrRotatorCase = Array.from(rotatorCase);
+let lengthArr = arrRotatorCase.length;
+let i = 0;
+
+setInterval(() => {
+  arrRotatorCase[i].classList.remove("rotator__case_active");
+  if (i === lengthArr - 1) {
+    i = 0;
+    arrRotatorCase[i].classList.add("rotator__case_active");
+  } else {
+    ++i;
+    arrRotatorCase[i].classList.add("rotator__case_active");
+  }
+}, 1000);
